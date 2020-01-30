65352cd6feea1aaaa2cff25237a78fbffdc7fd70
veryfatwombat <veryfatwombat@gmail.com>
2020-01-30 20:51:37 +1100

    coronavirus

--- 3/js/js.js
+++ 3/js/js.js
@@ -4,12 +4,21 @@ $(function() {
 		introH = $("#intro").innerHeight();
 		scrollOffset = 0;
 
+	checkScroll(scrollOffset);
+
 	$(window).on("scroll", function() {
 
 		scrollOffset = $(this).scrollTop();
 
-		console.log(scrollOffset);
-
+		checkScroll(scrollOffset);
 	});
+
+	function checkScroll(scrollOffset) {
+		if( scrollOffset >= introH ) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	}
 
 });
