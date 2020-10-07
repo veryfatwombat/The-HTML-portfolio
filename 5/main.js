306d92e2fe33a65d323e90786df1b0c72064d2e2
veryfatwombat <veryfatwombat@gmail.com>
2020-10-07 21:45:40 +1100

    css

--- 5/main.js
+++ 5/main.js
@@ -14,4 +14,10 @@ $(document).ready(function () {
             scrollTop: $($(this).attr('href')).offset().top - 100
         }, options: 2000);
     });
+
+    $('#up').on('click', function () {
+        $('html, body').animate(keyframes{
+            scrollTop: 0
+        }, options: 2000);
+    });
 });
